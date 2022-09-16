d20ba0071d319fb3418f9491537ebfcd19bdcb8c
Ux1ew1 <uxlewl0220@gmail.com>
2022-09-16 20:51:11 +0300

    ProApple

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,14 @@
 import Swiper from '../lib/swiper-bundle.esm.browser.min.js';
 
+// simplebar
+new SimpleBar(document.querySelector('.country__list'), {
+    classNames: {
+        scrollbar: 'country__scrollbar',
+        track: 'country__track',
+    }
+})
+
+// slider 
 new Swiper('.goods__block', {   //активируем Swiper 
     slidesPerView: 1,           //колличество отображаемых слайдов 
     spaceBetween: 20,           //расстояние между соседними слайдами 
@@ -22,9 +31,91 @@ new Swiper('.goods__block', {   //активируем Swiper
     navigation: {           //подключаем кнопки 
         prevEl: 'goods__arrow_prev',
         nextEl: 'goods__arrow_next',
+    },
+    preventClicks: true,    // убираем клик по кнопкам "узнать больше"
+    a11y: false,            //
+});
+
+// modal 
+const productMore = document.querySelectorAll('.product__more');
+const modal = document.querySelector('.modal');
+const formPlaceholder = document.querySelectorAll('.form__placeholder');
+const formInput = document.querySelectorAll('.form__input');
+
+productMore.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        modal.classList.add('modal_open')
+    })
+});
+
+modal.addEventListener('click', ({target}) => {
+    if (target === modal) {
+        modal.classList.remove('modal_open')
     }
 });
 
+formInput.forEach((input, i) => {
+    input.addEventListener('focus', () => {
+        formPlaceholder[i].classList.add('form__placeholder_active');
+    })
+    input.addEventListener('blur', () => {
+        if (input.value === '') {
+            formPlaceholder[i].classList.remove('form__placeholder_active');
+        }
+    })
+})
 
 
 
+// currency 
+
+const dataCurrency = {};
+
+const formatCurrency = (value, currency) => {
+    return new Intl.NumberFormat('EU', {
+        style: 'currency', 
+        currency, 
+        maximumFractionDigits: 2, 
+    }).format(value);
+}
+
+const showPrice = (currency = 'USD') => {
+    const priceElems = document.querySelectorAll('[data-price]')
+
+    priceElems.forEach(elem => {
+        elem.textContent = formatCurrency(elem.dataset.price * dataCurrency[currency], currency);
+    })
+}
+
+const myHeaders = new Headers();
+myHeaders.append("apikey", "07hkodHH1xfqKXXgZ4K1TCzJl50F5Wml");
+
+const requestOptions = {
+  method: 'GET',
+  redirect: 'follow',
+  headers: myHeaders
+};
+
+fetch("https://api.apilayer.com/fixer/latest?base=USD", requestOptions)
+  .then(response => response.json())
+  .then(result => {
+    Object.assign(dataCurrency, result.rates)
+    showPrice();
+  })
+  .catch(error => console.log('error', error));
+
+  // choises 
+
+const countryBtn = document.querySelector('.country__btn');
+const countryWrapper = document.querySelector('.country__wrapper');
+
+countryBtn.addEventListener('click', () => {
+    countryWrapper.classList.toggle('country__wrapper_open')
+});
+
+countryWrapper.addEventListener('click', ({target}) => {
+    if (target.classList.contains('country__choise')) {
+        countryWrapper.classList.remove('country__wrapper_open');
+        showPrice(target.dataset.currency);
+    }
+});
